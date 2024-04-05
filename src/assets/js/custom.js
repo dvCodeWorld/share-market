dc0a96542644cf9681da75c3077a986b7e2bbec5
Divyanshu Shrivastava <divyanshushrivastava123@gmail.com>
2024-04-06 02:41:46 +0530

    risk form integration

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -14,16 +14,62 @@ window.addEventListener("scroll", event => {
     }
 });
 
-function contactUs(event) {
+function getFormValue(event, formId) {
     event.preventDefault();
 
-    const form = document.getElementById('contactUsForm');
-    const contactUsform = new FormData(form);
+    const form = document.getElementById(formId);
+    const formDate = new FormData(form);
     const jsonData = {};
 
-    contactUsform.forEach((value, key) => {
+    formDate.forEach((value, key) => {
         jsonData[key] = value;
     });
     console.log(jsonData);
+    return jsonData;
+}
 
+
+
+// Risk Analysis Form
+let currentStep = 1;
+
+function changeStep(isStepOne) {
+    document.getElementById(`step-${currentStep}`).classList.remove('active-step');
+    isStepOne ? currentStep++ : currentStep--;
+    document.getElementById(`step-${currentStep}`).classList.add('active-step');
+}
+
+function submitRiskAnalysisForm(event, formId, isStepOne = true) {
+    const form = document.getElementById(formId);
+    if(form.checkValidity()) {
+        getFormValue(event, formId);
+        changeStep(isStepOne);
+    } else{
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    form.classList.add('was-validated')
+}
+
+
+// Function to validate the PAN Number 
+// 
+function isValidPanCardNo(panCardNo) {
+    let regex = new RegExp(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/);
+ 
+    // if PAN Number 
+    // is empty return false
+    if (panCardNo == null) {
+        return "false";
+    }
+ 
+    // Return true if the PAN NUMBER
+    // matched the ReGex
+    if (regex.test(panCardNo) == true) {
+        return "true";
+    }
+    else {
+        return "false";
+    }
 }
