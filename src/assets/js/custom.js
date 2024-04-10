102990c4065c44a758c0782489ab099580734f75
Divyanshu Shrivastava <divyanshushrivastava123@gmail.com>
2024-04-11 01:18:16 +0530

    change name

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -105,16 +105,20 @@ function handleFormChange() {
       lowRiskWrapper.innerHTML = lowRiskWrapperContent;
       moderateRiskWrapper.innerHTML = "";
       highRiskWrapper.innerHTML = "";
+      window.riskFormObject.options.riskStatus = riskSegmemt.value;
     } else if (riskCount >= 18 && riskCount <= 22) {
       riskSegmemt.value = "Medium - Preferred Service- Stock Cash";
       lowRiskWrapper.innerHTML = "";
       moderateRiskWrapper.innerHTML = moderateRiskWrapperContent;
       highRiskWrapper.innerHTML = "";
+      window.riskFormObject.options.riskStatus = riskSegmemt.value;
+
     } else {
       riskSegmemt.value = "High - Preferred Service Future, Option, Commodity";
       lowRiskWrapper.innerHTML = "";
       moderateRiskWrapper.innerHTML = moderateRiskWrapperContent;
       highRiskWrapper.innerHTML = highRiskWrapperContent;
+      window.riskFormObject.options.riskStatus = riskSegmemt.value;
     }
   }
 }
@@ -122,13 +126,99 @@ function handleFormChange() {
 function submitSelectServicesForm(event, formId) {
   riskFormObject.options.businessName = getById("question21").value;
   riskFormObject.selectedServices = getFormValue(event, formId);
-  console.log("riskFormObject", riskFormObject);
+
+  let services = Object.keys(riskFormObject.selectedServices).
+  filter(i=> i !== 'payableAmount')
+  .map(x => 
+    x.replaceAll('_',' ').replaceAll('-','/'));
+
+  let questions =  '';
+  window.riskAnalysisQuestions.forEach((question,index) => {
+    questions += `<tr>
+      <td width="80px">${index + 1}</td>
+      <td width="44%">${question}</td>
+      <td width="44%">${window.riskFormObject.options.choices['q'+(index+1)] || 'N/A'}</td>
+    </tr>`
+  })
+  console.log("questions",questions);
+  let printAbleContent = `
+  <div class="fs-14 p-5 text-primary-emphasis border border-secondary-subtle border-1" >
+        <h1 class="text-center mb-5" style="font-size: 28px;font-weight: 700;">Risk Analysis Form Details</h1>
+
+        <table width="100%" class="table table-bordered">
+            <tbody>
+                <tr>
+                    <td class="fw-medium">Client Name:</td>
+                    <td>${riskFormObject.userInfo.name}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Contact:</td>
+                    <td>${riskFormObject.userInfo.contact}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Pan Number:</td>
+                    <td>${riskFormObject.userInfo.panNo}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Aadhar Number:</td>
+                    <td>${riskFormObject.userInfo.adharCardNo}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Date of Birth:</td>
+                    <td>${riskFormObject.userInfo.dob}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Email:</td>
+                    <td>${riskFormObject.userInfo.email}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Current City:</td>
+                    <td>${riskFormObject.userInfo.city}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Service:</td>
+                    <td>${services}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Payable Amount:</td>
+                    <td>${riskFormObject.selectedServices.payableAmount}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Risk Status:</td>
+                    <td>${riskFormObject.options.riskStatus}</td>
+                </tr>
+                <tr>
+                    <td class="fw-medium">Total Scores:</td>
+                    <td>${riskFormObject.options.score}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="text-center mt-5 pt-3">
+            <hr>
+            <span class="fs-24 d-inline-block bg-white position-relative" style="transform: translateY(-100%);">Submited Question Answer</span>
+        </div>
+        <table width="100%" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th class="bg-info-subtle bg-gradient" width="80px">Q No.</th>
+                    <th class="bg-info-subtle bg-gradient" width="44%">Questions</th>
+                    <th class="bg-info-subtle bg-gradient" width="44%">Answer (Score)</th>
+                </tr>
+            </thead>
+            <tbody>`
+                +
+                questions
+                +
+            `</tbody>
+        </table>
+    </div>`;
+
+    printSection(printAbleContent);
 }
 
-function printSection(sectionId) {
-  const printContents = document.getElementById(sectionId).innerHTML;
+function printSection(printAbleContent) {
   const originalContents = document.body.innerHTML;
-  document.body.innerHTML = printContents;
+  document.body.innerHTML = printAbleContent;
   // Print the specific div
   window.print();
   // Restore original page content
--- src/assets/js/global.constant.js
+++ src/assets/js/global.constant.js
@@ -4,7 +4,8 @@ window.riskFormObject = {
         businessName: null,
         choices: null,
         selectedChoiceCount: null,
-        score: null
+        score: null,
+        riskStatus: null
     },
     selectedServices: null
 };
@@ -38,7 +39,7 @@ window.lowRiskWrapperContent =
     <div class="row row-cols-1 row-gap-2">
         <div class="col">
             <label for="lowRisk">
-                <input type="radio" class="form-check-input" id="lowRisk" name="lowRisk" value="true">
+                <input type="radio" class="form-check-input" id="lowRisk" name="No_Service_can_be_offered" value="true">
                 <span>No Service can be offered</span>
             </label>
         </div>
@@ -50,7 +51,7 @@ window.moderateRiskWrapperContent =
     <div class="row row-cols-1 row-gap-2">
         <div class="col">
             <label for="moderateRisk">
-                <input type="radio" class="form-check-input" id="moderateRisk" name="moderateRisk" value="true">
+                <input type="radio" class="form-check-input" id="moderateRisk" name="Stock_Cash" value="true">
                 <span>Stock Cash</span>
             </label>
         </div>
@@ -62,38 +63,79 @@ window.highRiskWrapperContent =
     <div class="row row-cols-1 row-gap-2">
         <div class="col">
             <label for="highRiskServices1">
-                <input type="radio" class="form-check-input" id="highRiskServices1" name="indexOption" value="true">
+                <input type="radio" class="form-check-input" id="highRiskServices1" name="Index_Option" value="true">
                 <span>Index Option</span>
             </label>
         </div>
         <div class="col">
             <label for="highRiskServices2">
-                <input type="radio" class="form-check-input" id="highRiskServices2" name="highRiskServices" value="true">
+                <input type="radio" class="form-check-input" id="highRiskServices2" name="Stock_Option" value="true">
                 <span>Stock Option</span>
             </label>
         </div>
         <div class="col">
             <label for="highRiskServices3">
-                <input type="radio" class="form-check-input" id="highRiskServices3" name="highRiskServices" value="true">
+                <input type="radio" class="form-check-input" id="highRiskServices3" name="Stock_Future" value="true">
                 <span>Stock Future</span>
             </label>
         </div>
         <div class="col">
             <label for="highRiskServices4">
-                <input type="radio" class="form-check-input" id="highRiskServices4" name="highRiskServices" value="true">
+                <input type="radio" class="form-check-input" id="highRiskServices4" name="Bluechip_C-F-O" value="true">
                 <span>Bluechip C/F/O</span>
             </label>
         </div>
         <div class="col">
             <label for="highRiskServices5">
-                <input type="radio" class="form-check-input" id="highRiskServices5" name="highRiskServices" value="true">
+                <input type="radio" class="form-check-input" id="highRiskServices5" name="Commodity_Service" value="true">
                 <span>Commodity Service</span>
             </label>
         </div>
         <div class="col">
             <label for="highRiskServices6">
-                <input type="radio" class="form-check-input" id="highRiskServices6" name="highRiskServices" value="true">
+                <input type="radio" class="form-check-input" id="highRiskServices6" name="Wealth_Management_C-F-O-Commodity" value="true">
                 <span>Wealth Management C/F/O/Commodity</span>
             </label>
         </div>
     </div>`;
+
+let x = {
+    "userInfo": {
+        "name": "Divyanshu Shrivastava",
+        "contact": "9111525164",
+        "panNo": "JFYPS1602Q",
+        "adharCardNo": "900866257575",
+        "dob": "1999-11-14",
+        "email": "divyanshu@gmail.com",
+        "city": "Anuppur"
+    },
+    "options": {
+        "businessName": "Dhjdisd",
+        "choices": {
+            "q1": "21 to 35 Year (4)",
+            "q2": "Professional (2)",
+            "q3": "5 to 10 Lac (3)",
+            "q4": "Long term (1)",
+            "q5": "2 to 5 Lac (1)",
+            "q6": "Extensive Experience (4)",
+            "q7": "Very Less Experience(1)",
+            "q8": "Average(2)",
+            "q9": "Regular Income(2)",
+            "q10": "Less then 2 years(1)",
+            "q11": "Less then 1 Lac(1)",
+            "q12": "All(4)",
+            "q13": "Less then 3 months(1)",
+            "q14": "20 to 35%(2)",
+            "q17": "40% and above(4)",
+            "q18": "I want to but I fail to save(2)",
+            "q20": "Connected to Media(0)"
+        },
+        "selectedChoiceCount": 17,
+        "score": 35
+    },
+    "selectedServices": {
+        "moderateRisk": "true",
+        "highRiskServices": "true",
+        "payableAmount": "2000"
+    }
+};
